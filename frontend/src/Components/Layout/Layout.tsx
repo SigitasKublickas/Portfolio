b9d2800fc506e202bccf498d086ba737888d3b6a
Sigitas <sig.kublickas99@gmail.com>
2024-02-23 00:31:37 +0200

    add Footer and Outlt to layout

--- frontend/src/Components/Layout/Layout.tsx
+++ frontend/src/Components/Layout/Layout.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Categories from "../Categories/Categories";
 import Header from "../Header/Header";
+import Home from "../../Pages/Home/Home";
+import Footer from "../Footer/Footer";
+import { Outlet } from "react-router";
 
 type Props = {};
 
 const Layout = (props: Props) => {
   return (
-    <div className="layout">
+    <div className="layout flex flex-col center">
       <Header />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
