66cc23133da73fe6979435b63811e0d3a3d7fd8a
Sigitas <sig.kublickas99@gmail.com>
2024-02-23 00:31:53 +0200

    add css to home page

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Home.css";
 
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  return <main className="container">Home</main>;
 };
 
 export default Home;
